ef68b9aee0c3e700b3e307b836c6c9c684628913
Artem Vasyukhin <73034094+Vasart-ds@users.noreply.github.com>
2023-05-09 01:09:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 st.title('_Jules Winnfield CEFR english predicting_')
 welcome_img, welcome_text = st.columns(2)
 welcome_img = welcome_img.image('./img/welcome_image.jpg')
-welcome_text = welcome_text.markdown('**_Sup fella. Ya know where ya come? Here we talkin about CEFR levels - the system of knowin foreign languages. If ya know me, then u like to watch movies and ya know Im talking in English, dearfrend. Wanna try some? Push da button below, upload subs of ur best movie and enjoy!_**')
+welcome_text = welcome_text.markdown('**_Do you know what this place is? Here you can evaluate the difficulty of the movie you selected for learning a foreign language. If you recognise me, you know Im an expert in English, so Ill be the one helping you with this. Hit the button below, upload the subtitles and find out if the movie you chose fits your English level._**')
 
 movie_name, upload_button, free_space_2 = st.columns(3)
 upload_button = upload_button.file_uploader(label='English, dearfrend, can u read it?!', accept_multiple_files = False)
