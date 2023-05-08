b851cfbb9ecf933de8a3f6b5e1dc94af8493966a
Artem Vasyukhin <73034094+Vasart-ds@users.noreply.github.com>
2023-05-08 11:12:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,5 @@ import os
 
 # content
 st.title('_Samuel L. Jackson CERF english predicting_')
-
+welcome_img, welcome_text = st.columns(2)
+welcome_img = welcome_img.image('https://cdn.mos.cms.futurecdn.net/shuXAAqrMyYzUVMEusFTpm-1200-80.jpg')
