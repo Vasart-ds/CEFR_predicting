c17b0e67b7a3c4b5575be007f8289bf75a5c24e9
Artem Vasyukhin <73034094+Vasart-ds@users.noreply.github.com>
2023-05-08 20:42:34 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,9 +84,9 @@ if upload_button is not None:
     text = prepare_text(string_data)
     text = del_stopwords(text)
     #text = nltk.word_tokenize(text, language = "english")
-    spacy_results = nlp(text)
+    #spacy_results = nlp(text)
     #text = ' '.join([token.lemma_ for token in spacy_results])
-    st.write(spacy_results)
+    st.write(text)
     
     
     
