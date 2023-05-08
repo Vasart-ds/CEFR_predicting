fae27add5aff1804242de4c6e34baa7739419266
Artem Vasyukhin <73034094+Vasart-ds@users.noreply.github.com>
2023-05-08 23:16:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,7 +106,7 @@ if upload_button is not None:
     
     # ВАРИАНТ 3
     data = preprocess(string_data)
-    data = nltk.word_tokenize(data, language = "english")
+    #data = nltk.word_tokenize(data, language = "english")
     st.write(data)
     #vectorizer = CountVectorizer(stop_words = stop_words)
     #vectorized_sub = vectorizer.fit_transform(data).toarray()
