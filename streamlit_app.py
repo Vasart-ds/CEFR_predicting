44bea6e76e06a7f79dc5cb45287f1dd2499ac5b3
Artem Vasyukhin <73034094+Vasart-ds@users.noreply.github.com>
2023-05-08 17:52:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ welcome_img = welcome_img.image('./img/welcome_image.jpg')
 welcome_text = welcome_text.markdown('**_Sup fella. Ya know where ya come? Here we talkin about CEFR levels - the system of knowin foreign languages. If ya know me, then u like to watch movies and ya know Im talking in English, dearfrend. Wanna try some? Push da button below, upload subs of ur best movie and enjoy!_**')
 
 free_space_1, upload_button, free_space_2 = st.columns(3)
-upload_button = upload_button.st.file_uploader(label='English, dearfrend, can u read it?!')
+upload_button = upload_button.file_uploader(label='English, dearfrend, can u read it?!')
 
 if upload_button is not None:
   stringio = StringIO(upload_button.getvalue().decode('iso-8859-1'))
