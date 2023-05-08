29d092ec98c999332f68a4b4d4986de7a1201fd6
Artem Vasyukhin <73034094+Vasart-ds@users.noreply.github.com>
2023-05-09 00:06:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,7 +87,8 @@ if upload_button is not None:
     text = prepare_text(string_data)
     text = del_stopwords(text)
     text = nlp(text)
-    text = ' '.join([token.lemma_ for token in text])
+    #text = ' '.join([token.lemma_ for token in text])
+    text = lemmatize(text)
     text = pd.Series(text)
     
     vectorizer = CountVectorizer(stop_words=stop_words)
