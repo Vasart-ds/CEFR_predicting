9beaa0f9f47afc77b3f36806af8da2a3063b44ed
Artem Vasyukhin <73034094+Vasart-ds@users.noreply.github.com>
2023-05-08 16:54:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import os
 st.title('_Samuel L. Jackson CEFR english predicting_')
 welcome_img, welcome_text = st.columns(2)
 welcome_img = welcome_img.image('./img/welcome_image.jpg')
-welcome_text = welcome_text.markdown('**Sup fella. Ya know where ya come? Here we talkin about CEFR levels - the system of knowin foreign languages. \n
-                                      If ya know me, then u like to watch movies and ya know Im talking in English, dearfrend. \n
-                                      Wanna try some? Push da button below, upload subs of ur best movie and enjoy!**'
+welcome_text = welcome_text.markdown('**Sup fella. Ya know where ya come? Here we talkin about CEFR levels - the system of knowin foreign languages.',
+                                      'If ya know me, then u like to watch movies and ya know Im talking in English, dearfrend.',
+                                      'Wanna try some? Push da button below, upload subs of ur best movie and enjoy!**'
                                      )
