123f2bfc6110030f7310b21f3663d9bd0a29b99c
Artem Vasyukhin <73034094+Vasart-ds@users.noreply.github.com>
2023-05-09 00:30:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ if upload_button is not None:
     text = prepare_text(string_data)
     text = del_stopwords(text)
     text = nltk.word_tokenize(text,language = "english")
-    text = nlp(str(text))
+    #text = nlp(str(text))
     text = ' '.join([token.lemma_ for token in text])
     
     subs_features = pd.DataFrame({'subtitles': [text]})
