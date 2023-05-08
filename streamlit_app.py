5de46779c5f85981fd12102095503aeaf8e41a94
Artem Vasyukhin <73034094+Vasart-ds@users.noreply.github.com>
2023-05-08 23:47:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,7 +89,7 @@ if upload_button is not None:
     text = nlp(text)
     text = ' '.join([token.lemma_ for token in text])
     
-    vectorizer = CountVectorizer(stopwords=stop_words)
+    vectorizer = CountVectorizer(stop_words=stop_words)
     vectorized_sub = vectorizer.fit_transform([text]).toarray()
     st.write(vectorized_sub)
     
