b7df7ccca56541cef7c19d8e62ac5f2a825b1a35
Artem Vasyukhin <73034094+Vasart-ds@users.noreply.github.com>
2023-05-09 00:10:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,8 +97,8 @@ if upload_button is not None:
     
 #    df = pd.DataFrame({})
     
-    #model = pickle.load(open(r'catboost_clf.pcl', 'rb'))
-    #prediction = model.predict(vectorized_sub)
-    #st.write(prediction)
+    model = pickle.load(open(r'catboost_clf.pcl', 'rb'))
+    prediction = model.predict(vectorized_sub)
+    st.write(prediction)
     
     #movie_name = movie_name.write(prediction_clf)
